f24d60ce21ba50d41be82e98502f8f81b1b3f9c5
gumbi2400 <joel@tapdaq.com>
2019-09-21 11:03:17 +0100

    added missing provider and token statements

--- hcloud/hcloud-network/variables.tf
+++ hcloud/hcloud-network/variables.tf
@@ -5,3 +5,7 @@ variable "network_name" {
 variable "ip_range" {
   description = "IP range of the network"
 }
+
+variable "hcloud_token" {
+  description = "The token to be used to connect to the Hertzner Cloud account"
+}
